c8c2de772ca07087ec4355576b9a7df8c2de048d
SergeyBocharov-test <work.sergey.bocharov@ya.ru>
2021-03-15 14:51:35 +0300

    feat: implemented requred function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (typeof(array) !== ('undefined' || null) ){
+    return array.length != 0 ? Math.min.apply(null, array) : 0
+  } else {
+    return 0
+  } 
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof(array) !== ('undefined' || null) ){
+    return array.length != 0 ? Math.max.apply(null, array) : 0
+  } else {
+    return 0
+  } 
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  const sum = (a, b) => a + b
+  if (typeof(array) !== ('undefined' || null) ){
+    return array.length != 0 ? array.reduce(sum) / array.length : 0
+  } else {
+    return 0
+  }
 }
